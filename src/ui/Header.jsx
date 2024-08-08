39de1fc3e7e9d2e9965e08e96c8605e3bc6f39ae
Arvin Buid <arvinbuid01@gmail.com>
2024-08-08 23:16:29 +0800

    Created AppLayout and Header component. Modified App component createBrowserRouter into making AppLayout the layout route or the parent of all the routes inside the application.

---
+++ src/ui/Header.jsx
@@ -0,0 +1,13 @@
+import {Link} from "react-router-dom";
+
+function Header() {
+  return (
+    <header>
+      <Link to='/'>Pizza Palooza</Link>
+
+      <p>Arvin</p>
+    </header>
+  );
+}
+
+export default Header;
