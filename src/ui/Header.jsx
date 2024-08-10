70fbba197940fbc67dc5afcf4c74e44e6a71ad7a
Arvin Buid <arvinbuid01@gmail.com>
2024-08-11 00:30:43 +0800

    Text styling.

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,12 +1,16 @@
 import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder";
+import Username from "../features/user/Username";
 
 function Header() {
   return (
-    <header className="bg-yellow-500">
-      <Link to="/">Pizza Palooza</Link>
+    <header className="bg-yellow-500 uppercase">
+      <Link to="/" className="tracking-widest">
+        Pizza Palooza
+      </Link>
       <SearchOrder />
-      <p>Arvin</p>
+
+      <Username />
     </header>
   );
 }
