14cd7841f65c59d4f4f2763ab2526a242d3b8bdf
Arvin Buid <arvinbuid01@gmail.com>
2024-08-10 17:50:42 +0800

    Implement a loading indicator inside the form button in CreateOrder component using useNavigation() hook from react router.

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,5 +1,5 @@
 import {useState} from "react";
-import {Form, redirect} from "react-router-dom";
+import {Form, redirect, useNavigation} from "react-router-dom";
 import {createOrder} from "../../services/apiRestaurant";
 
 // https://uibakery.io/regex-library/phone-number
@@ -33,6 +33,8 @@ const fakeCart = [
 function CreateOrder() {
   // const [withPriority, setWithPriority] = useState(false);
   const cart = fakeCart;
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === "submitting";
 
   return (
     <div>
@@ -71,7 +73,7 @@ function CreateOrder() {
 
         <div>
           <input type='hidden' name='cart' value={JSON.stringify(cart)} />
-          <button>Order now</button>
+          <button disabled={isSubmitting}>{isSubmitting ? "Placing Order..." : "Order now"}</button>
         </div>
       </Form>
     </div>
