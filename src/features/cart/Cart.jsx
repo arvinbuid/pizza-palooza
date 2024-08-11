f3d19a8463bc1cf06265f8e69af78d582b930ed8
Arvin Buid <arvinbuid01@gmail.com>
2024-08-11 21:54:51 +0800

    Added styling inside Cart & CartItem component.

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 import LinkButton from '../../ui/LinkButton';
 import Button from '../../ui/Button';
+import CartItem from './CartItem';
 
 const fakeCart = [
   {
@@ -30,7 +31,7 @@ function Cart() {
   const cart = fakeCart;
 
   return (
-    <div>
+    <div className="px-4 py-3">
       <LinkButton
         to="/menu"
         className="text-sm text-blue-500 hover:text-blue-600 hover:underline"
@@ -38,9 +39,15 @@ function Cart() {
         &larr; Back to menu
       </LinkButton>
 
-      <h2>Your cart, %NAME%</h2>
+      <h2 className="mt-8 text-xl font-semibold">Your cart, %NAME%</h2>
 
-      <div>
+      <ul className="mt-3 divide-y-2 divide-stone-200 border-b-2">
+        {cart.map((item) => (
+          <CartItem item={item} key={item.pizzaId} />
+        ))}
+      </ul>
+
+      <div className="mt-6 space-x-2">
         <Button to="/order/new" type="primary">
           Order pizzas
         </Button>
