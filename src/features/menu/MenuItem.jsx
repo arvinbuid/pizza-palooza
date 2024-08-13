330005cdd5f5f264d1cb16ead604c2ad520e9177
Arvin Buid <arvinbuid01@gmail.com>
2024-08-13 23:02:39 +0800

    Added UpdateItemQuantity reusable button component inside MenuItem component to show item quantity & increase/decrease the quantity of the pizza.

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -2,6 +2,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { formatCurrency } from '../../utils/helpers';
 import { addItem, getCurrentQuantityById } from '../cart/cartSlice';
 import DeleteItem from '../cart/DeleteItem';
+import UpdateItemQuantity from '../cart/UpdateItemQuantity';
 import Button from '../../ui/Button';
 
 function MenuItem({ pizza }) {
@@ -43,7 +44,15 @@ function MenuItem({ pizza }) {
             </p>
           )}
 
-          {isInCart && <DeleteItem pizzaId={id} />}
+          {isInCart && (
+            <div className="flex items-center gap-3 sm:gap-7">
+              <UpdateItemQuantity
+                pizzaId={id}
+                currentQuantity={currentQuantity}
+              />{' '}
+              <DeleteItem pizzaId={id} />
+            </div>
+          )}
 
           {!soldOut && !isInCart && (
             <Button type="small" onClick={handleAddToCart}>
